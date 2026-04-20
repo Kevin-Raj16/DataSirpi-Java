1161f0904c66de748c6d7530c1426aefcaf55c96
Kevin-Raj16 <kevinraj592@gmail.com>
2026-04-20 15:33:41 +0530

    Assignment Example bank balance

---
+++ AssignmentExample.java
@@ -0,0 +1,12 @@
+// Real-world example: Bank transactions
+public class AssignmentExample {
+    public static void main(String[] args) {
+
+        int balance = 1000;
+
+        balance += 500; // deposit
+        balance -= 200; // withdraw
+
+        System.out.println("Final Balance: " + balance);
+    }
+}
