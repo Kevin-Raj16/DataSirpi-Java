fbb1c926035773fcbb2386acc24c85cdde99cc8f
Kevin-Raj16 <kevinraj592@gmail.com>
2026-04-20 15:31:47 +0530

    Relational Operator Age comparision

---
+++ RelationalExample.java
@@ -0,0 +1,9 @@
+// Real-world example: Voting eligibility
+public class RelationalExample {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        System.out.println("Eligible for vote: " + (age >= 18));
+    }
+}
