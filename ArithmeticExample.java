bcfb26027b4ac8569d8955219aae079868037371
Kevin-Raj16 <kevinraj592@gmail.com>
2026-04-20 15:30:44 +0530

    Arithmetic Operator Example

---
+++ ArithmeticExample.java
@@ -0,0 +1,16 @@
+// Real-world example: Shopping bill calculation
+public class ArithmeticExample {
+    public static void main(String[] args) {
+
+        int price = 500;
+        int quantity = 3;
+
+        int total = price * quantity; // multiplication
+        int discount = 100;
+
+        int finalAmount = total - discount; // subtraction
+
+        System.out.println("Total: " + total);
+        System.out.println("Final Amount: " + finalAmount);
+    }
+}
